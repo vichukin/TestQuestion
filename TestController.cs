588e83c7f4f2918c4e7ffa8b5ba703c21a8a166a
Dmytro <64381710+vichukin@users.noreply.github.com>
2024-06-03 18:21:56 +0200

    Add files via upload

---
+++ TestController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace TestQuestion.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpPost("Delete/{id}")]
+        public void Delete(uint id)
+        {
+            User user = _context.Users.FirstOrDefault(user => user.Id == id);
+            if(user == null) return NotFound("Wrong id");
+            _context.Users.Remove(user);
+            _context.SaveChanges();
+            Debug.WriteLine($"The User with login={user.login} has been deleted");
+            return Ok();
+        }
+    }
+    //Trzeba było sprawdzić czy istnieje użytkownik z takim Id
+}
